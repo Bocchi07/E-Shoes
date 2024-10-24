addd23a24c1e8eb80c2a713478222cc7c323fa68
Jeaven A. Paras <jeavenanda07@gmail.com>
2024-10-23 22:26:16 -1100

    Add vercel.json for routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
-      require('tailwind-scrollbar', 'daisyui'),
-  ],
+    require('tailwind-scrollbar')
+    // require('daisyui'),
+  ]
 }
-
